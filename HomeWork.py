60f0582e96a8e7de5d35f7693f63a93d655555d1
DmitriyGanockijj <d.ganockiy@gost.team>
2024-09-23 13:01:44 +0300

    Update HomeWork.py

--- HomeWork.py
+++ HomeWork.py
@@ -1,2 +1,6 @@
 char = input('Введите символ: ')
 print (char, char + char, char+char+char, char+char+char+char, char+char+char+char+char,sep = '\n')
+
+import calendar
+month_number = int(input('Введите номер месяца: '))
+print(calendar.month_name[month_number])
