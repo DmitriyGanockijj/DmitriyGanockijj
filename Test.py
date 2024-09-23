09ea8bd3d33ed8c428d5ebf990e3ddd7b1f2d9d3
DmitriyGanockijj <d.ganockiy@gost.team>
2024-09-23 12:06:08 +0300

    Update Test.py
    DZ

--- Test.py
+++ Test.py
@@ -1,5 +1,23 @@
-def main():
-    print("Скрипт успешно запущен.")
+char = input('Введите символ: ')
+print (char)
+print (char + char)
+print (char + char + char)
+print (char + char + char + char)
+print (char + char + char + char + char)
 
-if __name__ == "__main__":
-    main()
+month_name = {1: 'Январь',
+              2: 'Февраль',
+              3: 'Март',
+              4: 'Апрель',
+              5: 'Май',
+              6: 'Июнь',
+              7: 'Июль',
+              8: 'Август',
+              9: 'Сентябрь',
+              10: 'Октябрь',
+              11: 'Ноябрь',
+              12: 'Декабрь'
+              }
+
+month_number = int(input ('Введите номер месяца: '))
+print (month_name.get(month_number))
